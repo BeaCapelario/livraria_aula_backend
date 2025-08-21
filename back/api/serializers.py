a34e8dad2ae41432f6a2c49386120d56e63ba345
Beatriz Feitosa Capelario <42400785805@>
2025-08-21 11:32:32 -0300

    21/08

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from .models import Autor
+from .models import Autor, Editora, Livro
 
 class AutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Autor
         fields = '__all__' #todos os arquivos
-        
+
+class EditoraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = '__all__'
+
+class LivroSerializer(serializers.ModelSerializer):
+    editora = EditoraSerializer(read_only=True)
+    editora_id = serializers.PrimaryKeyRelatedField(
+        queryset = Editora.objects.all(), source ='editora', write_only = True)
+    
+    autor = AutorSerializer(read_only=True)
+    autor_id = serializers.PrimaryKeyRelatedField(
+        queryset = Autor.objects.all(), source ='autor', write_only = True)
+    
+    class Meta:
+        model = Livro
+        fields = ['id','titulo', 'subtitulo', 'autor', 'autor', 'editora', 'editora', 'isbn', 'descricao', 'idioma', 'ano_publicacao', 'paginas', 'preco', 
+                  'estoque', 'desconto', 'disponivel', 'dimensoes', 'peso', 'editora', 'editora_id', 'autor', 'autor_id']
+
+        def __str__(self):
+            return self.editora
--- back/api/urls.py
+++ back/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import AutoresView, listar_autores
+from .views import AutoresView, listar_autores, EditoraView, LivroView
 
 urlpatterns = [
-    path('autores' , AutoresView.as_view()) ,
-    path('authors' , listar_autores)
+    path('autores' , AutoresView.as_view()),
+    path('authors' , listar_autores),
+    path('editoras' , EditoraView.as_view()),
+    path('livros', LivroView.as_view()),
 ]
 
